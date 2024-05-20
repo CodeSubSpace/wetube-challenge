39f42f07fe5fc31c4ba0eb21eed7dc8417d28361
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-20 15:21:40 +0900

    DAY 15 Blueprint

--- src/db.js
+++ src/db.js
@@ -1,65 +1,22 @@
-// DONT TOUCH THIS FILE <3
-
-import axios from "axios";
-let movies = [];
-const YIFY_URL = "https://yts.mx/api/v2/";
-const client = axios.create({
-  baseURL: YIFY_URL
-});
-const startDB = async () => {
-  try {
-    console.log("⏳  Starting Movie DB");
-    ({
-      data: {
-        data: { movies }
-      }
-    } = await client.get("/list_movies.json", { params: { limit: 50 } }));
-    console.log("✅  Movie DB Ready!");
-  } catch (e) {
-    console.log(e.message);
-    console.log("❌ Can't initialize DB, contact Nico");
+import mongoose from "mongoose";
+/*
+DONT TOUCH THIS FILE <3
+WE ARE ALL SHARING THE SAME DB
+PLEASE DONT TOUCH THIS FILE
+*/
+mongoose.connect(
+  "mongodb+srv://wetubeadmin:wetube4ever@cluster0.uufes.mongodb.net/wetube?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
   }
-};
-startDB();
+);
 
-// This gives you an array of all the movies
-export const getMovies = () => movies;
+const db = mongoose.connection;
 
-// This gives you one movie, don't forget to pass the ID
-export const getMovieById = (id) => {
-  if (!id) {
-    throw Error("❌  YOU FORGOT TO PASS THE MOVIE ID TO THE FUNCTION  ❌ ");
-  }
-  return movies.find((m) => m.id === parseInt(id, 10));
-};
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
 
-// This gives you an array of movies with a release date of minimum X
-export const getMovieByMinimumYear = (year) => {
-  if (!year) {
-    throw Error("❌  YOU FORGOT TO PASS THE MOVIE YEAR TO THE FUNCTION  ❌");
-  }
-  return movies.filter((m) => m.year >= year);
-};
-
-// This gives you an array of movies with a rating of minimum X
-export const getMovieByMinimumRating = (rating) => {
-  if (!rating) {
-    throw Error("❌  YOU FORGOT TO PASS THE MOVIE RATING TO THE FUNCTION  ❌");
-  }
-  return movies.filter((m) => m.rating >= rating);
-};
-
-export const addMovie = ({ title, synopsis, genres }) => {
-    console.log(title)
-    console.log(synopsis)
-    console.log(genres)
-    if (typeof title !== "string" || typeof synopsis !== "string") {
-      throw Error("❌  title and synopsis should be strings  ❌");
-    }
-    if (!genres instanceof Array) {
-      throw Error("❌  genres should be an array  ❌");
-    }
-    const id = Math.floor(Math.random() * (title.length + Date.now()));
-    movies = [{ id, title, synopsis, genres }, ...movies];
-  };
-  
+db.once("open", handleOpen);
+db.on("error", handleError);
--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
+import "./db";
+import "./models/Movie";
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
-import movieRouter from "./routers/movieRouter";
-import addRouter from "./routers/addRouter";
-import { localsMiddleware } from "./middlewares/middlewares";
-
+import movieRouter from "./movieRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
-const PORT = 4000;
-
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-app.set("views", process.cwd() + "/src/views");
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(localsMiddleware);
-
-//routers
 app.use("/", movieRouter);
-app.use("/add", addRouter);
 
-const listenHandler = (req, res) => {
-    console.log(`Server listening on http://localhost:${PORT}`)
-}
-app.listen(PORT, listenHandler)
+// Codesanbox does not need PORT :)
+app.listen(() => console.log(`✅  Server Ready!`));
